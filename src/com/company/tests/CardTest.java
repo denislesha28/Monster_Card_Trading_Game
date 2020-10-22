1bfe51448e80c6fc46561f826c1e3f46c33f4c97
Denis Lesha <denislesha8@gmail.com>
2020-10-22 19:05:28 +0200

    First CardTest Class

---
+++ src/com/company/tests/CardTest.java
@@ -0,0 +1,11 @@
+package com.company.tests;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+public class CardTest {
+    @Test
+    public void testCardType(){
+
+    }
+}
