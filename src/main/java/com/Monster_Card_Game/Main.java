1c4d34f8afc4cfc4b5b4643a3c83cef7e27489dc
Denis Lesha <denislesha8@gmail.com>
2020-11-29 23:59:08 +0100

    Base Socket Infrastructure

--- src/main/java/com/Monster_Card_Game/Main.java
+++ src/main/java/com/Monster_Card_Game/Main.java
@@ -2,8 +2,12 @@ package com.Monster_Card_Game;
 
 import com.Monster_Card_Game.server.RequestContext;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
 
@@ -12,6 +16,12 @@ public class Main {
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
             RequestContext handler = new RequestContext();
+            Socket clientSocket = serverSocket.accept();
+            if (clientSocket != null) {
+                //System.out.println("Connected");
+            }
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         }catch (IOException e){
             System.out.println(e);
         }
