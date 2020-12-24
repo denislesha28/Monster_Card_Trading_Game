b6c3adfd74ae541d0f20e7b23e7c8c77c9a05ff9
Denis Lesha <denislesha8@gmail.com>
2020-12-24 22:54:41 +0100

    Hashing and Validating Passwords

--- src/main/java/com/Monster_Card_Game/server/DatabaseHandler.java
+++ src/main/java/com/Monster_Card_Game/server/DatabaseHandler.java
@@ -1,6 +1,8 @@
 package com.Monster_Card_Game.server;
 
 import java.net.ConnectException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,6 +13,8 @@ public class DatabaseHandler {
     String username;
     String password;
     Connection connection;
+    PasswordHasher pwHasher=new PasswordHasher();
+
     public DatabaseHandler() throws SQLException {
         jdbcURL="jdbc:postgresql://localhost:5432/MonsterCardGame";
         username="postgres";
@@ -18,7 +22,8 @@ public class DatabaseHandler {
         connection = DriverManager.getConnection(jdbcURL,username,password);
         System.out.println("Database Connected");
     }
-    public void createUser(String username,String password) throws SQLException {
+    public void createUser(String username,String password) throws SQLException, InvalidKeySpecException, NoSuchAlgorithmException {
+        password=pwHasher.generateStorngPasswordHash(password);
         String insertStatement="INSERT INTO \"MonsterCardGame\".\"user\" (\"username\",\"password\") " +
                 "VALUES (\'"+username+"\',\'"+password+"\')";
         System.out.println(insertStatement);
