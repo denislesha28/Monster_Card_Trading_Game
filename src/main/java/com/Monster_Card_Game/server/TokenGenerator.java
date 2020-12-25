bf54a484197a1da912619b3a0e84abf05c4e1f4d
Denis Lesha <denislesha8@gmail.com>
2020-12-25 21:29:16 +0100

    1st Version Token Generator

---
+++ src/main/java/com/Monster_Card_Game/server/TokenGenerator.java
@@ -0,0 +1,21 @@
+package com.Monster_Card_Game.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TokenGenerator {
+    List<String> tokens = new ArrayList<String>();
+    public String generateToken(String input){
+        String token="Basic "+input+"-mtcgToken";
+        tokens.add(token);
+        return token;
+    }
+
+    public boolean authenticate(String input){
+        String token="Basic "+input+"-mtcgToken";
+        if(tokens.contains(token)){
+            return true;
+        }
+        return false;
+    }
+}
